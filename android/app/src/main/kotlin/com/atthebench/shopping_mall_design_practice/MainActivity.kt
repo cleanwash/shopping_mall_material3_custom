9402e56f4ea3f1dc314d5f1f9397fd5ff771a5c0
Jayone Lee <kenbugless@gmail.com>
2025-03-01 15:03:04 +0900

    feat:res(색상, 폰트) 추가

---
+++ android/app/src/main/kotlin/com/atthebench/shopping_mall_design_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.atthebench.shopping_mall_design_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/util/router.dart
@@ -0,0 +1,8 @@
+import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+final router = GoRouter(
+  initialLocation: '/', // 앱 시작 시 초기 경로
+  routes: [],
+);
